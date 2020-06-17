c51ac8439603de735cf9d1ae9d6ec83fe22ed591
tag200mph <jonathanscraig@gmail.com>
2020-06-16 22:12:49 -0700

    revisions to index.html and script.js
    git status
    
    a

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-//defining global variables
-var getPercentageChange = "";
-var oldNumber = "";
-var newNumber = "";
-
-//function to check (+/-10%) of BTC value
-function getPercentageChange() {
-  var currentValue = oldNumber - newNumber;
-  return (currentValue / oldNumber) * 100;
-}
-
-function giphyPrint() {
-  let result;
-  if (getPercentageChange >= 10) {
-    result = "positive";
-  } else {
-    result = "NOT positive";
-  }
-  return result;
-}
 // Adding click event listen listener to all buttons
 $("button").on("click", function () {
   // Grabbing and storing the data-currency property value from the button
@@ -25,9 +5,7 @@ $("button").on("click", function () {
 
   // Constructing a queryURL using the currency name
   var queryURL =
-    "https://api.giphy.com/v1/gifs/search?q=" +
-    currency +
-    "&api_key=dc6zaTOxFJmzC&limit=10";
+    "https://api.bitcoincharts.com/v1/markets.json" + currency + "";
 
   // Performing an AJAX request with the queryURL
   $.ajax({
@@ -42,9 +20,27 @@ $("button").on("click", function () {
       // storing the data from the AJAX request in the results variable
       var results = response.data;
 
-      // Looping through each result item
-      for (var i = 0; i < results.length; i++) {
-        // Creating and storing a div tag
+
+//function to check (+/-10%) of BTC value
+function getPercentageChange() {
+    var currentValue = oldNumber - newNumber;
+    return (currentValue / oldNumber) * 100;
+  }
+  
+  //function to print gif happy/sad
+  function giphyPrint() {
+    let result;
+    if (getPercentageChange >= 10) {
+      result = "http://api.giphy.com/v1/gifs/search?q=money+laughing+cash+rich+startup&api_key=9VZTsKtrM0yPVEY7JlmcICxPTh31NxSQ&limit=1";
+    } else {
+      result = "http://api.giphy.com/v1/gifs/search?q=spongebob squarepants+sad+spongebob&api_key=9VZTsKtrM0yPVEY7JlmcICxPTh31NxSQ&limit=1";
+    }
+    return result;
+  }
+      // We will not be looping through anything? Looping through each result item
+      //for (var i = 0; i < results.length; i++) {
+        
+      // Creating and storing a div tag
         var currencyDiv = $("<div>");
 
         // Creating and storing an image tag
@@ -53,7 +49,6 @@ $("button").on("click", function () {
         currencyImage.attr("src", results[i].images.fixed_height.url);
 
         // Appending the paragraph and image tag to the currencyDiv
-        //currencyDiv.append(p);
         currencyDiv.append(currencyImage);
 
         // Prependng the currencyDiv to the HTML page in the "#gifs-appear-here" div
