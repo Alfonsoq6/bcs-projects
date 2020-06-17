5a131df1f4b1f2799ea982b29f2fd8d5688857b5
tag200mph <jonathanscraig@gmail.com>
2020-06-16 20:49:28 -0700

    revisions to index.html and script.js

--- script.js
+++ script.js
@@ -9,12 +9,6 @@ function getPercentageChange() {
   return (currentValue / oldNumber) * 100;
 }
 
-// Performing an AJAX request with the queryURL
-$.ajax({
-  url: queryURL,
-  method: "GET",
-});
-
 function giphyPrint() {
   let result;
   if (getPercentageChange >= 10) {
@@ -24,7 +18,53 @@ function giphyPrint() {
   }
   return result;
 }
+// Adding click event listen listener to all buttons
+$("button").on("click", function () {
+  // Grabbing and storing the data-currency property value from the button
+  var currency = $(this).attr("data-currency");
 
-console.log(giphyPrint(11));
-console.log(giphyPrint);
-console.log(getPercentageChange);
+  // Constructing a queryURL using the currency name
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?q=" +
+    currency +
+    "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  // Performing an AJAX request with the queryURL
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  })
+    // After data comes back from the request
+    .then(function (response) {
+      console.log(queryURL);
+
+      console.log(response);
+      // storing the data from the AJAX request in the results variable
+      var results = response.data;
+
+      // Looping through each result item
+      for (var i = 0; i < results.length; i++) {
+        // Creating and storing a div tag
+        var currencyDiv = $("<div>");
+
+        // Creating a paragraph tag with the result item's rating
+        //var p = $("<p>").text("Rating: " + results[i].rating);
+
+        // Creating and storing an image tag
+        var currencyImage = $("<img>");
+        // Setting the src attribute of the image to a property pulled off the result item
+        currencyImage.attr("src", results[i].images.fixed_height.url);
+
+        // Appending the paragraph and image tag to the currencyDiv
+        //currencyDiv.append(p);
+        currencyDiv.append(currencyImage);
+
+        // Prependng the currencyDiv to the HTML page in the "#gifs-appear-here" div
+        $("#gif-here").prepend(currencyDiv);
+      }
+    });
+});
+
+//console.log(giphyPrint(11));
+//console.log(giphyPrint);
+//console.log(getPercentageChange);
