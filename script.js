9e9da45293e8cf2cc6e7f58bf047f39fdbb9f7b8
tag200mph <jonathanscraig@gmail.com>
2020-06-16 19:00:00 -0700

    Update to index.html and created script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+//function to check (+/-10%) of BTC value then print according gif
+//aqf will be changed to match Alfonso's math function variable name
+function percentChange(aqf) {
+  let result;
+  if (a > 0) {
+    result = "positive";
+  } else {
+    result = "NOT positive";
+  }
+  return result;
+}
+
+console.log(testNum(-5));
